18b21b9796ba6d9a62b5d180c56fb61da7a43055
Zorawar Singh <zorawar87@gmail.com>
2015-08-03 11:51:46 -0500

    final upload before eoc

--- PythonScripts/milestone2.py
+++ PythonScripts/milestone2.py
@@ -3,8 +3,8 @@ import cv2
 imageNames = "Blowhole013.jpg Pioneer0067.jpg frankenstein.jpg Blowhole021.jpg Pioneer0093.jpg garden.jpg Coins1.jpg Puzzle1.jpg ghostrider.jpg Coins2.jpg Puzzle2.jpg gorge.jpg DollarCoin.jpg SnowLeo1.jpg redDoor.jpg SnowLeo2.jpg sampleRed.jpg Pioneer0005.jpg SnowLeo3.jpg shops.jpg Pioneer0012.jpg SnowLeo4.jpg swans.jpg Pioneer0029.jpg frame0017.jpg waterfall.jpg Pioneer0063.jpg frame0019.jpg"
 imageNames = imageNames.split(' ')
 
-for i in images:
-	print(i)
+for i in imageNames:
+	print("Now Showing: "+i)
 	cimg = cv2.imread("../TestImages/"+i)
 	cv2.imshow("Image", cimg)
 	cv2.waitKey(0)
---
+++ PythonScripts/script5.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy
+
+image = cv2.imread("../TestImages/SnowLeo4.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray image", gray)
+blackImg = numpy.zeros((150, 250), numpy.uint8)
+cv2.imshow("Blank image", blackImg)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
